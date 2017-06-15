19165662e6d8e526b00746bca0f7430b7bacac03
LK <307832539@qq.com>
2017-06-15 23:04:12 +0800

    日志初步显示

--- src/main/java/com/entity/Content.java
+++ src/main/java/com/entity/Content.java
@@ -6,7 +6,7 @@ import com.entity.base.BaseContent;
  * 内容类
  * Created by LK on 2017/5/7.
  */
-public class Content extends BaseContent{
+public class Content extends BaseContent {
     private static final long serialVersionUID = 1L;
 
     public Content() {
